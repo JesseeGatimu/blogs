f3e0c6ebf8498b49c728a97f82fbc6c7a76baada
JesseeGatimu <gakuojessee@gmail.com>
2026-09-20 23:42:28 +0300

    Added comment model

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -18,10 +18,18 @@ class Post(models.Model):
     title=models.CharField()
     author=models.ForeignKey(User,on_delete=models.CASCADE)
     content=models.TextField()
-    category=models.ForeignKey(Categories,on_delete=models.CASCADE)
+    categories=models.ForeignKey(Categories,on_delete=models.CASCADE,related_name="categories")
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
-    
+
+class Comments(models.Model):
+    post=models.ForeignKey(Post,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    content=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user} about {self.post}"

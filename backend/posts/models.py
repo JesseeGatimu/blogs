542b39de97e5b4b80304149a4bf4002ef31f57a0
JesseeGatimu <gakuojessee@gmail.com>
2026-09-21 08:22:45 +0300

    Created serializer for the posts app

--- backend/posts/models.py
+++ backend/posts/models.py
@@ -25,7 +25,7 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-class Comments(models.Model):
+class Comment(models.Model):
     post=models.ForeignKey(Post,on_delete=models.CASCADE)
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     content=models.TextField()
